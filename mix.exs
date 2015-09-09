891f338310ead245fc0091ed48e2ca8cb788c8c4
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-09-10 00:27:42 +0900

    add some mix and license

--- mix.exs
+++ mix.exs
@@ -1,32 +1,37 @@
 defmodule ExParametarized.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
-    [app: :ex_parametarized,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :ex_parametarized,
+      version: @version,
+      elixir: "~> 1.0",
+      name: "ExParametarized",
+      source_url: "https://github.com/KazuCocoa/ex_parametarized",
+      description: "Simple macro for parametarized testing",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: package
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      files: ~w(lib config mix.exs README.md LICENSE),
+      contributors: ["Kazuaki Matsuo"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/KazuCocoa/ex_parametarized"}
+    ]
   end
 end
