d8847023db3fdd3740b1b941f6d7d473aa54fcb1
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-09-10 01:41:28 +0900

    revert unexpected replacing Keyword to Dict

--- lib/ex_parametarized/params.ex
+++ lib/ex_parametarized/params.ex
@@ -2,7 +2,7 @@ defmodule ExUnit.Parametarized.Params do
   @moduledoc false
 
   defmacro test_with_params(desc, fun, params) do
-    Dict.get(params, :do, nil)
+    Keyword.get(params, :do, nil)
     |> param_with_index
     |> Enum.map(fn({{_, _, values}, i})->
          quote do
