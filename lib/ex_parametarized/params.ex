018d5187dae7200cdf0692837fcfefb9eb14a1c5
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-09-20 12:00:16 +0900

    refactor test_with

--- lib/ex_parametarized/params.ex
+++ lib/ex_parametarized/params.ex
@@ -10,39 +10,28 @@ defmodule ExUnit.Parametarized.Params do
   end
 
   defp test_with(desc, fun, {{param_desc, {_, _, values}}, num}) when is_atom(param_desc) do
-    quote do
-      test unquote("#{desc}_#{param_desc}_num#{num}") do
-        unquote(fun).(unquote_splicing(values))
-      end
-    end
-  end
-
-  defp test_with(desc, fun, {{_, _, values}, num}) do
-    quote do
-      test unquote("#{desc}_num#{num}") do
-        unquote(fun).(unquote_splicing(values))
-      end
-    end
+    run("#{desc}_#{param_desc}_num#{num}", fun, values)
   end
 
   defp test_with(desc, fun, {{param_desc, values}, num}) when is_atom(param_desc) do
-    quote do
-      test unquote("#{desc}_#{param_desc}_num#{num}") do
-        unquote(fun).(unquote_splicing(Tuple.to_list(values)))
-      end
-    end
+    run("#{desc}_#{param_desc}_num#{num}", fun, Tuple.to_list(values))
+  end
+
+  defp test_with(desc, fun, {{_, _, values}, num}) do
+    run("#{desc}_num#{num}", fun, values)
   end
 
   defp test_with(desc, fun, {values, num}) do
+    run("#{desc}_num#{num}", fun, Tuple.to_list(values))
+  end
+
+  defp run(desc, fun, params) do
     quote do
-      test unquote("#{desc}_num#{num}") do
-        unquote(fun).(unquote_splicing(Tuple.to_list(values)))
-      end
+      test unquote(desc), do: unquote(fun).(unquote_splicing(params))
     end
   end
 
   defp param_with_index(list) do
     Enum.zip list, 0..Enum.count(list)
   end
-
 end
