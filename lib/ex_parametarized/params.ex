a6bf902122a27595f757d04cf4583cad6e877660
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-09-22 10:25:34 +0900

    add comment

--- lib/ex_parametarized/params.ex
+++ lib/ex_parametarized/params.ex
@@ -14,17 +14,15 @@ defmodule ExUnit.Parametarized.Params do
     run("#{desc}_#{param_desc}_num#{num}", fun, values)
   end
 
+  # Quote literals case : http://elixir-lang.org/docs/master/elixir/Kernel.SpecialForms.html#quote/2
   defp test_with(desc, fun, {{param_desc, values}, num}) when is_atom(param_desc) do
     run("#{desc}_#{param_desc}_num#{num}", fun, Tuple.to_list(values))
   end
 
-  defp test_with(desc, fun, {{_, _, values}, num}) do
-    run("#{desc}_num#{num}", fun, values)
-  end
+  defp test_with(desc, fun, {{_, _, values}, num}), do: run("#{desc}_num#{num}", fun, values)
 
-  defp test_with(desc, fun, {values, num}) do
-    run("#{desc}_num#{num}", fun, Tuple.to_list(values))
-  end
+  # Quote literals case : http://elixir-lang.org/docs/master/elixir/Kernel.SpecialForms.html#quote/2
+  defp test_with(desc, fun, {values, num}), do: run("#{desc}_num#{num}", fun, Tuple.to_list(values))
 
   defp run(desc, fun, params) do
     quote do
