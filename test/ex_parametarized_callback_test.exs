de582137abed9fee7abf087d4a9b1188693eabf5
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-10-03 08:49:15 +0900

    add casetest template

---
+++ test/ex_parametarized_callback_test.exs
@@ -0,0 +1,119 @@
+defmodule ExParametarizedParamsCallbackTest do
+  use ExUnit.Case, async: true
+  use ExUnit.Parametarized
+
+  setup do
+    {:ok, [hello: "world", value: 1, bool: false]}
+  end
+
+  test "ast format when one param with context" do
+    import ExUnit.Parametarized.ParamsCallback
+    assert (
+      quote do
+        test_with_params "ast test", context, fn (a) -> a == "test" end do
+          [{"test"}]
+        end
+      end
+      |> Macro.to_string) == String.strip ~S"""
+        test_with_params("ast test", context, fn a -> a == "test" end) do
+          [{"test"}]
+        end
+        """
+  end
+
+  test "ast format when two param with context" do
+    import ExUnit.Parametarized.ParamsCallback
+    assert (
+      quote do
+        test_with_params "ast test", context, fn (a, b) -> assert a + b == 2 end do
+          [{1, 2}]
+        end
+      end
+      |> Macro.to_string) == String.strip ~S"""
+        test_with_params("ast test", context, fn a, b -> assert(a + b == 2) end) do
+          [{1, 2}]
+        end
+        """
+  end
+
+  @tag skip: "If failed to skip, test will fai"
+  test_with_params "skipped test with context", context,
+    fn (a) ->
+      assert a == true
+    end do
+      [
+        {context[:bool]}
+      ]
+  end
+
+  test_with_params "provide one param with context", context,
+    fn (a) ->
+      assert a == 1
+    end do
+      [
+        {context[:value]},
+        "two values": {context[:value]}
+      ]
+  end
+
+  test_with_params "compare two values with context", context,
+    fn (a, expected) ->
+      assert a == expected
+    end do
+      [
+        {context[:value], 1},
+        "two values": {context[:hello], "world"}
+      ]
+  end
+
+  test_with_params "add params with context", context,
+    fn (a, b, expected) ->
+      assert a + b == expected
+    end do
+      [
+        {context[:value], 2, 3}
+      ]
+  end
+
+  test_with_params "create wordings with context", context,
+    fn (a, b, expected) ->
+      str = a <> " and " <> b
+      assert str == expected
+    end do
+      [
+        {context[:hello], "cats", "world and cats"},
+        {"hello", context[:hello], "hello and world"}
+      ]
+  end
+
+  test_with_params "fail case with context", context,
+    fn (a, b, expected) ->
+      refute a + b == expected
+    end do
+      [
+        {context[:value], 2, 2}
+      ]
+  end
+
+  test_with_params "add description for each params with context", context,
+    fn (a, b, expected) ->
+      str = a <> " and " <> b
+      assert str == expected
+    end do
+      [
+        "description for param1": {context[:hello], "cats", "world and cats"},
+        "description for param2": {"hello", context[:hello], "hello and world"}
+      ]
+  end
+
+  test_with_params "mixed no desc and with desc for each params with context", context,
+    fn (a, b, expected) ->
+      str = a <> " and " <> b
+      assert str == expected
+    end do
+      [
+        {context[:hello], "cats", "world and cats"}, # no description
+        "description for param2": {"hello", context[:hello], "hello and world"} # with description
+      ]
+  end
+end
